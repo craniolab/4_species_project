66bf6ab1cb6cd2bec8ff1b7fdd5ddf1316414fd2
craniolab <seton@evolbio.mpg.de>
2025-04-07 09:20:59 +0200

    save notebooks and commit

--- save_notebooks_commit_push.sh
+++ save_notebooks_commit_push.sh
@@ -7,6 +7,17 @@ for filename in *.ipynb; do
 	jupyter nbconvert $filename --to notebook \
 		--ClearOutputPreprocessor.enabled=True --output ../notebooks_github/$filename
 done
+
+for dir in */; do
+	for filename in $dir/*.ipynb; do
+		mkdir -p ../notebooks_github/$dir
+		pandoc --listings -H listings-setup.tex -V geometry:paperwidth=8in -V geometry:paperheight=200in -V geometry:margin=1in \
+		--to pdf --from ipynb -o ../notebooks_github/$dir/$filename.pdf $filename
+
+		jupyter nbconvert $filename --to notebook \
+			--ClearOutputPreprocessor.enabled=True --output ../notebooks_github/$dir/$filename
+	done
+done
 cd ..
 git add -A
 git commit -m "save notebooks and commit"
